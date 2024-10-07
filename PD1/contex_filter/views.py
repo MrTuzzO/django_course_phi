7cae899218109cc3dbbeb2daf0d11d2cbd501f76
MrTuzzO <khirulislam5750@gmail.com>
2024-10-07 19:56:02 +0600

    more practice added

--- PD1/contex_filter/views.py
+++ PD1/contex_filter/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(request):
@@ -7,5 +8,7 @@ def home(request):
         "age" : 22,
         "marks" : [70, 60, 86, 91, 56, 80],
         "friends": ["Karim", "Zoy", "Pial", "Nayeem", "Najmul"],
+        "currentTime" : datetime.datetime.now(),
+        "sentence":"hello, i am learning pyhton django framework."
     }
     return render(request, 'index.html',person)
