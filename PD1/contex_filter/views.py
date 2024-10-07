0c661f0b3d4a746e9470a6862c600bc0afcaf345
MrTuzzO <khirulislam5750@gmail.com>
2024-10-07 19:13:21 +0600

    filter practice added

--- PD1/contex_filter/views.py
+++ PD1/contex_filter/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    person = {
+        "name" : "Khirul Islam",
+        "age" : 22,
+        "marks" : [70, 60, 86, 91, 56, 80],
+        "friends": ["Karim", "Zoy", "Pial", "Nayeem", "Najmul"],
+    }
+    return render(request, 'index.html',person)
